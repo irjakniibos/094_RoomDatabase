b28b1e11f6f4bc7696c748d7743471536de4cde3
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-11-27 13:31:10 +0700

    Menambahkan objek DestinasiEntry untuk routing navigasi entry siswa.

---
+++ app/src/main/java/com/example/a20230140094_roomdatabase/view/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.a20230140094_roomdatabase.view.route
+
+import com.example.a20230140094_roomdatabase.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
