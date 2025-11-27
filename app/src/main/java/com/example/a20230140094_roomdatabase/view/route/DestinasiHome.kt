97a13c2342839fcf82cf92e1c826c5e4e61f7190
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-11-27 13:28:23 +0700

    Menambahkan objek DestinasiHome untuk navigasi.

---
+++ app/src/main/java/com/example/a20230140094_roomdatabase/view/route/DestinasiHome.kt
@@ -0,0 +1,10 @@
+package com.example.a20230140094_roomdatabase.view.route
+
+import com.example.a20230140094_roomdatabase.R
+
+object DestinasiHome : DestinasiNavigasi {
+
+    override val route = "home"
+
+    override val titleRes = R.string.app_name
+}
