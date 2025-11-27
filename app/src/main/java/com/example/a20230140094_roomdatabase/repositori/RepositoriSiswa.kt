cc1554c87747dfcd590e3d9a878838ccf22d7c00
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-11-27 13:11:52 +0700

    Menambahkan interface RepositoriSiswa dengan fungsi getAllSiswaStream dan insertSiswa.

--- app/src/main/java/com/example/a20230140094_roomdatabase/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/a20230140094_roomdatabase/repositori/RepositoriSiswa.kt
@@ -1,2 +1,12 @@
 package com.example.a20230140094_roomdatabase.repositori
 
+import com.example.a20230140094_roomdatabase.room.Siswa
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa{
+
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+
+    suspend fun insertSiswa(siswa: Siswa)
+}
+
